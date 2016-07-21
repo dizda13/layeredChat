26faaeed2d198d833e0d22c957f6522d8503407d
dizda13 <dizda13@github.com>
2016-07-21 13:27:05 +0200

    geteri, seter konstruktor u socketConnection

--- src/main/java/tranferLayer/socketConection.java
+++ src/main/java/tranferLayer/socketConection.java
@@ -12,6 +12,35 @@ public class socketConection {
     private String ip;
     private Socket socket;
 
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public socketConection(String ip, String port){
+        this.ip=ip;
+        this.port=port;
+    }
+
     public String connect() throws IOException, IllegalArgumentException  {
         if(ip=="")
             throw new IllegalArgumentException("You didn't set IP adress");
